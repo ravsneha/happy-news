d2256b935d612c122dab4acb321c71d06ce56ba0
Sneha Ravichandran <sneha.rcd@gmail.com>
2020-05-02 14:53:41 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@
 var url = 'http://newsapi.org/v2/everything?' + 'q=Apple&' + 'from=2020-04-30&' + 'sortBy=popularity&' + 'apiKey=abb657dbf02941289a4e3c844b2f644c';
 var req = new Request(url);
 fetch(req).then(function (response) {
-  console.log(response.json());
+  console.log(response);
 });
