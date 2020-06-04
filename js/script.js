9442fec280ce5593b55cd4322044f20124b72527
Sneha Ravichandran <sneha.rcd@gmail.com>
2020-06-04 16:34:01 -0700

    Delete script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var url = 'http://newsapi.org/v2/everything?' + 'q=Apple&' + 'from=2020-04-30&' + 'sortBy=popularity&' + 'apiKey=abb657dbf02941289a4e3c844b2f644c';
-var req = new Request(url);
-fetch(req).then(function (response) {
-  console.log(response);
-});
